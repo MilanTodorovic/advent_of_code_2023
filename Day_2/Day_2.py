38050d3db33edc0a8a699045a2227a8e6a387030
MilanTodorovic <todor-90@hotmail.com>
2024-12-02 19:53:37 +0100

    Added Day_2 part two solution

--- Day_2/Day_2.py
+++ Day_2/Day_2.py
@@ -44,9 +44,30 @@ def part_one():
 	print("Possible games:", sum(possible_games))
 
 def part_two():
-	pass
-
+	power = []
+	with open("./input.txt", "r") as file:
+		for line in file.readlines():
+			red, green, blue = [[],[],[]]
+			_, rest = line.split(":")
+			draws = rest.split(";")
+			for draw in draws:
+				draw = draw.strip()
+				cubes = draw.split(",")
+				for cube in cubes:
+					cube = cube.strip()
+					r = cube.find("red")
+					if r >= 0:
+						red.append(int(cube[0:r-1]))
+					g = cube.find("green")
+					if g >= 0:
+						green.append(int(cube[0:g-1]))
+					b = cube.find("blue")
+					if b >= 0:
+						blue.append(int(cube[0:b-1]))
+			
+			power.append(max(red)*max(green)*max(blue))
+	print("Sum of powers:", sum(power))
 
 if __name__ == "__main__":
-	part_one()
+	# part_one()
 	part_two()
